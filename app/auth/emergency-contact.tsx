41d9e0f1b5203e1d5c5fbdee1f60a44b1415eeef
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-09-15 21:06:50 -0700

    added placeholder rnfes for emergency contact

---
+++ app/auth/emergency-contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EmergencyContact = () => {
+  return (
+    <View>
+      <Text>Emergency Contact</Text>
+    </View>
+  )
+}
+
+export default EmergencyContact
+
+const styles = StyleSheet.create({})
