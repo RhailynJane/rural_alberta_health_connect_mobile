3c178acffd1745651385579f501170a69b08a944
Yue Zhou <amirzhou001@gmail.com>
2025-09-18 09:32:19 -0600

    feat: add firstName/lastName fields to users table schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,6 +4,17 @@ import { v } from "convex/values";
 
 export default defineSchema({
   ...authTables,
+  users: defineTable({
+    email: v.string(),
+    firstName: v.string(),
+    lastName: v.string(),
+    emailVerificationTime: v.optional(v.number()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.number()),
+    isAnonymous: v.optional(v.boolean()),
+    name: v.optional(v.string()),
+    image: v.optional(v.string()),
+  }).index("email", ["email"]),
   userProfiles: defineTable({
     userId: v.id("users"),
     ageRange: v.optional(v.string()),
