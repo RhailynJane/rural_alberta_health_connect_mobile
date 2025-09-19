2f61b3e29e13f841d2f283aee6c83c0b337809a0
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-09-18 17:22:09 -0700

    created functin=on for handlign emergency call

--- app/(tabs)/tracker/index.tsx
+++ app/(tabs)/tracker/index.tsx
@@ -1,19 +1,38 @@
-import { Text, View } from 'react-native';
+import { Alert, Text, View } from 'react-native';
 import BottomNavigation from '../../components/bottomNavigation';
 import CurvedBackground from '../../components/curvedBackground';
 import CurvedHeader from '../../components/curvedHeader';
 import { FONTS } from '../../constants/constants';
 
 export default function Tracker() {
+  const handleEmergencyCall = (): void => {
+    Alert.alert(
+      "Emergency Call",
+      "For life-threatening emergencies, call 911 immediately.",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Call 911",
+          onPress: () => {
+            console.log("Calling 911...");
+          },
+        },
+      ]
+    );
+  };
+  
   return (
     <CurvedBackground>
       <CurvedHeader
-        title="Tracker"
+        title="Health Tracker"
         height={120}
         showLogo={true}
       />
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontFamily: FONTS.BarlowSemiCondensed }}>Tracker Screen</Text>
+        <Text style={{ fontFamily: FONTS.BarlowSemiCondensed }}>Health Tracker Screen</Text>
       </View>
       <BottomNavigation />
     </CurvedBackground>
