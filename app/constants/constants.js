ec2d72441135dfff586d0e7cd1e6efb5f90a72af
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-09-18 20:37:10 -0700

    added colors in constant

--- app/constants/constants.js
+++ app/constants/constants.js
@@ -16,3 +16,13 @@ export const FONT_CONFIG = {
   BarlowSemiCondensedBold: Barlow_800ExtraBold,
   BarlowSemiCondensedExtraBold: Barlow_900Black, // Boldest option
 };
+
+export const COLORS = {
+  primary: '#007AFF',
+  white: '#FFFFFF',
+  black: '#000000',
+  darkText: '#1C1C1E',
+  lightGray: '#8E8E93',
+  darkGray: '#48484A',
+  darkCard: '#1C1C1E',
+};
