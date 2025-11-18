6190b988146c096445fecfabdf0a0f8ab4871cd2
RhailynJane <rhailynjane.cona@sait.edu.ca>
2025-11-17 23:13:53 -0700

    fix(android): update DummyDetectorInstaller to accept ReactContext base type

--- native/dummy-detector/android/src/main/java/com/amirzhou/rahcapp/DummyDetectorInstaller.kt
+++ native/dummy-detector/android/src/main/java/com/amirzhou/rahcapp/DummyDetectorInstaller.kt
@@ -1,18 +1,34 @@
 package com.amirzhou.rahcapp
 
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContext
 
 object DummyDetectorInstaller {
     init {
-        System.loadLibrary("dummy-detector")
+        try {
+            System.loadLibrary("dummy-detector")
+        } catch (_: UnsatisfiedLinkError) {
+            // Library may not be present in some build variants; ignore to avoid hard crash
+        }
     }
 
     @JvmStatic
     external fun install(runtimePtr: Long)
 
     @JvmStatic
-    fun installJSI(reactContext: ReactApplicationContext) {
-        val ptr = reactContext.javaScriptContextHolder.get()
-        install(ptr)
+    fun installJSI(reactContext: ReactContext) {
+        val ptr = try {
+            if (reactContext is ReactApplicationContext) {
+                reactContext.javaScriptContextHolder?.get() ?: 0L
+            } else 0L
+        } catch (_: Throwable) {
+            0L
+        }
+        if (ptr == 0L) return
+        try {
+            install(ptr)
+        } catch (_: UnsatisfiedLinkError) {
+            // Native symbol missing; likely no native lib in this build type
+        }
     }
 }
