d49e93123f89470894c223ff8c98398253ab0137
Yue Zhou@Paraflux <yue@paraflux.ca>
2025-11-24 15:42:35 -0700

    chore: pkg, includ onnx in metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 // Add support for .tflite files (TensorFlow Lite models)
-config.resolver.assetExts.push('tflite');
+config.resolver.assetExts.push("tflite");
+config.resolver.assetExts.push("onnx");
+
+console.log("[Metro Config] Asset extensions:", config.resolver.assetExts);
 
 module.exports = config;
