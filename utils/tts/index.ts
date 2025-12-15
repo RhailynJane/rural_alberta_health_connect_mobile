ab83d4822b864a8f6fbd2263dbf2a25e9d089417
Yue Zhou@Paraflux <yue@paraflux.ca>
2025-12-14 23:21:32 -0700

    checkpoint: modify utils/tts/index.ts - 23:21

---
+++ utils/tts/index.ts
@@ -0,0 +1,38 @@
+// TTS Module - On-device Text-to-Speech using Kokoro ONNX
+//
+// Usage:
+// import { useTTS, prepareNextStepsForTTS } from '@/utils/tts';
+//
+// const { status, speak, stop, download } = useTTS();
+// await speak("Hello world");
+
+export { default as KokoroOnnx, type StreamStatus } from './kokoroOnnx';
+export {
+  MODELS,
+  DEFAULT_MODEL_ID,
+  isModelDownloaded,
+  getDownloadedModels,
+  downloadModel,
+  deleteModel,
+  getModelInfo,
+  type ModelId
+} from './models';
+export {
+  VOICES,
+  DEFAULT_VOICE_ID,
+  RECOMMENDED_VOICES,
+  getVoiceData,
+  isVoiceDownloaded,
+  getVoiceInfo,
+  type VoiceId,
+  type VoiceInfo
+} from './voices';
+export {
+  useTTS,
+  prepareTextForTTS,
+  prepareNextStepsForTTS,
+  prepareRecommendationsForTTS,
+  type TTSStatus,
+  type UseTTSReturn,
+  type UseTTSOptions
+} from './useTTS';
